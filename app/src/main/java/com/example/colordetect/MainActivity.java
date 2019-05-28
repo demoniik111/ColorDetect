050b793c747e02999d0bd7352619997b9e4aa8c9
Dmitriy Zheltyy <dazheltyy@gmail.com>
2019-05-28 22:11:23 +0300

    Add loaded image resource

---
+++ app/src/main/java/com/example/colordetect/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.colordetect;
+
+import android.annotation.SuppressLint;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+    private ImageView imageView;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        int imageResource = getResources().getIdentifier("@drawable/navfamily", null, this.getPackageName());
+        imageView.setImageResource(imageResource);
+    }
+
+}
