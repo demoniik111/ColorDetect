89c5c27efa56952b8a76631d39c1deb15e2a0ec5
Dmitriy Zheltyy <dazheltyy@gmail.com>
2019-05-28 22:38:44 +0300

    Add first version of the color Detect

--- app/src/main/java/com/example/colordetect/MainActivity.java
+++ app/src/main/java/com/example/colordetect/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.colordetect;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
     private ImageView imageView;
+    private TextView textView;
+    View mColorView;
+    Bitmap bitmap;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -34,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    @SuppressLint("ResourceType")
+    @SuppressLint({"ResourceType", "ClickableViewAccessibility"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,9 +51,53 @@ public class MainActivity extends AppCompatActivity {
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         imageView = (ImageView)findViewById(R.id.imageView);
+        textView = findViewById(R.id.resultTv);
+        mColorView = findViewById(R.id.colorView);
 
         int imageResource = getResources().getIdentifier("@drawable/navfamily", null, this.getPackageName());
         imageView.setImageResource(imageResource);
+
+        imageView.setDrawingCacheEnabled(true);
+        imageView.buildDrawingCache(true);
+
+//        imageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(event )
+//            }
+//        });
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
+                    if ((int)event.getX() >= 0 && (int)event.getY()>=0){
+                        bitmap = imageView.getDrawingCache();
+
+                        int pixel = bitmap.getPixel((int)event.getX() < bitmap.getWidth() ? (int)event.getX() : 0, (int)event.getY() < bitmap.getHeight() ? (int)event.getY() : 0 );
+
+                        //getting RGB
+                        int r = Color.red(pixel);
+                        int g = Color.green(pixel);
+                        int b = Color.blue(pixel);
+
+                        //getting HEX
+                        String hex = "#" + Integer.toHexString(pixel);
+
+                        //устанавливаем background для view
+                        mColorView.setBackgroundColor(Color.rgb(r,g,b));
+
+                        //устанавливаем значения в textview
+                        textView.setText("RGB: " + r + ", " + g + ", " + b + "\nHEX: " + hex);
+                    } else {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        });
+
+
     }
 
 }
